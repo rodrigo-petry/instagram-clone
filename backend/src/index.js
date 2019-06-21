5e2ede500bd1343133b28928f3c1f670d94cf7f2
Rodrigo Petry <rodrigo.petry18@gmail.com>
2019-06-21 00:59:04 -0300

    Adicionando funcionalidade de conversao do formato da imagem

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://dev:gqwabc123@cluster0-jmnp9.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
