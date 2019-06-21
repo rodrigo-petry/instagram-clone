3de8ef691de5e263519975bb42ba3bda997e99e2
Rodrigo Petry <rodrigo.petry18@gmail.com>
2019-06-21 01:01:00 -0300

    Adicionando o CORS na aplicaçao para tornar acessivel

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://dev:gqwabc123@cluster0-jmnp9.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
 });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
