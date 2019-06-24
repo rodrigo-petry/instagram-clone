36460deceb7d867a7186997a0ccc8784ba023cf0
Rodrigo Petry <rodrigo.petry18@gmail.com>
2019-06-23 21:55:38 -0300

    Adicionando funcionalidade de Likes

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import io from 'socket.io-client'
 
 import './Feed.css';
 
@@ -8,64 +10,73 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+  state = {
+    feed: [],
+  };
+  
+  async componentDidMount () {
+    this.registerToSocket();
+
+    const response = await api.get('posts');
+
+    this.setState({ feed: response.data });
+  }
+
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
+
+    // post, like
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    })
+
+    socket.on('like', likedPost => {
+      this.setState({ 
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      });
+    })
+  }
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
+  }
+
   render () {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Rodrigo Petry</span>
-              <span className="place">Porto Alegre</span>
-            </div>
+        { this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-            <img src={more} alt="Mais" />
-          </header>
+              <img src={more} alt="Mais" />
+            </header>
 
-          <img src="http://localhost:3333/files/52824661_2093520984060749_1131466971181212685_n.jpg" />
+            <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-          <footer>
-            <div className="actions">
-              <img src={like} />
-              <img src={comment} />
-              <img src={send} />
-            </div>
+            <footer>
+              <div className="actions">
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="" />
+                </button>
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
 
-            <strong>54 curtidas</strong>
+              <strong>{post.likes} curtidas</strong>
 
-            <p>
-              Uma foto muito legal com a minha namorada!
-              <span>#girlfriend #love</span>
-            </p>
-          </footer>
-        </article>
-
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Rodrigo Petry</span>
-              <span className="place">Porto Alegre</span>
-            </div>
-
-            <img src={more} alt="Mais" />
-          </header>
-
-          <img src="http://localhost:3333/files/52824661_2093520984060749_1131466971181212685_n.jpg" />
-
-          <footer>
-            <div className="actions">
-              <img src={like} />
-              <img src={comment} />
-              <img src={send} />
-            </div>
-
-            <strong>87 curtidas</strong>
-
-            <p>
-              Uma foto muito legal com a minha namorada!
-              <span>#girlfriend #love</span>
-            </p>
-          </footer>
-        </article>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        )) }
       </section>
     );
   }
